93cbdcaa66b07e205db896b8b56aca9f522010cb
meggmumbi <margaretmumbi16@gmail.com>
2020-06-17 14:42:13 +0300

    Enhancing the android notekeeper app

--- app/src/main/java/com/mumbi/notes/NavigationActivity.java
+++ app/src/main/java/com/mumbi/notes/NavigationActivity.java
@@ -1,28 +1,28 @@
 package com.mumbi.notes;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
-import android.view.View;
+import java.util.List;
 
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-import java.util.List;
 
 public class NavigationActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -44,9 +44,13 @@ public class NavigationActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               startActivity(new Intent(NavigationActivity.this, NoteListActivity.class));
+               startActivity(new Intent(NavigationActivity.this, MainActivity.class));
             }
         });
+
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_notification, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_data_sync, false);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -64,13 +68,30 @@ public class NavigationActivity extends AppCompatActivity
     protected void onResume() {
         super.onResume();
         mNoteRecyclerAdapter.notifyDataSetChanged();
+        updateNaviHeader();
+
+    }
+
+    private void updateNaviHeader() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View headerview = navigationView.getHeaderView(0);
+        TextView name = (TextView)headerview.findViewById(R.id.text_user_name);
+        TextView email = (TextView)headerview.findViewById(R.id.text_user_email_address);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        String userName = pref.getString("user_display_name", "");
+        String  emailAdreess = pref.getString("user_email_address", "");
+
+        name.setText(userName);
+        email.setText(emailAdreess);
+
     }
 
 
     private void initializeDisplayContent() {
         recycleritems = (RecyclerView) findViewById(R.id.list_items);
         notesLayoutManager = new LinearLayoutManager(this);
-        courseLayoutManager = new GridLayoutManager(this,2);
+        courseLayoutManager = new GridLayoutManager(this, getResources().getInteger(R.integer.course_grid_span));
 
 
         List<NoteInfo> notes = DataManager.getInstance().getNotes();
@@ -129,6 +150,7 @@ public class NavigationActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
 
@@ -147,7 +169,8 @@ public class NavigationActivity extends AppCompatActivity
         } else if (id == R.id.nav_courses) {
           dispalyCourses();
         } else if (id == R.id.nav_share) {
-           handleSelection("Tell your friend about us");
+            //handleSelection("Tell your friend about us");
+            handleShare();
         } else if (id == R.id.nav_send) {
            handleSelection("send us feedback");
         }
@@ -155,6 +178,14 @@ public class NavigationActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private void handleShare() {
+
+        View view = findViewById(R.id.list_items);
+        Snackbar.make(view, "share to -" +
+                PreferenceManager.getDefaultSharedPreferences(this).getString("user_fav_social", ""),
+                Snackbar.LENGTH_LONG).show();
     }
 
     private void handleSelection(String message) {
